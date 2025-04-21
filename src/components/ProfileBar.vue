8323d7c25a2be8320efc5fe16dff29b8da033326
idarijul <m.haidarijl@gmail.com>
2025-04-21 16:16:47 +0700

    move assets to public

--- src/components/ProfileBar.vue
+++ src/components/ProfileBar.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="flex items-center gap-3">
         <div class="size-[55px] overflow-hidden rounded-full bg-gray-300 sm:size-[82px]">
-            <img src="/src/assets/images/pp.png" alt="pp" />
+            <img src="/assets/images/pp.png" alt="pp" />
         </div>
         <div class="leading-4 sm:leading-6">
             <h3 class="text-[13.3px] font-bold uppercase sm:text-[24px]">Haidar Ijlal</h3>
